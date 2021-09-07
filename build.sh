ac163e3a3c87189b91899b566e264d96c5e883ee
Dirk <67624338+zauberhaus@users.noreply.github.com>
2021-09-08 08:28:39 +1200

    clean up

--- build.sh
+++ build.sh
@@ -31,13 +31,3 @@ for arch in $ARCHS ; do
     echo "Build external-dns.$arch $VERSION" 
     go build -o ../build/external-dns.$arch -ldflags "$FLAGS"
 done
-
-exit 0
-
-GOARCH=arm64 CGO_ENABLED=0 go build -o ../build/operator.arm64 -ldflags "$FLAGS" -trimpath ./operator/cmd/manager/
-GOARCH=arm GOARM=7 CGO_ENABLED=0 go build -o ../build/operator.armv7 -ldflags "$FLAGS" -trimpath ./operator/cmd/manager/
-GOARCH=arm GOARM=6 CGO_ENABLED=0 go build -o ../build/operator.armv6 -ldflags "$FLAGS" -trimpath ./operator/cmd/manager/
-GOARCH=s390x  CGO_ENABLED=0 go build -o ../build/operator.s390x -ldflags "$FLAGS" -trimpath ./operator/cmd/manager/
-GOARCH=ppc64le  CGO_ENABLED=0 go build -o ../build/operator.ppc64le -ldflags "$FLAGS" -trimpath ./operator/cmd/manager/
-GOARCH=386 GO386='softfloat' CGO_ENABLED=0 go build -o ../build/operator.x86 -ldflags "$FLAGS" -trimpath ./operator/cmd/manager/
-CGO_ENABLED=0 go build -o ../build/operator.x86_64 -ldflags "$FLAGS" -trimpath ./operator/cmd/manager/
